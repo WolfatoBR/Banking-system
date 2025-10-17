379c076fc3eded425d505dd0f8e6386415ac78ce
Arthur Moreira da Silva <arthur.moreira@fbest.org.br>
2025-10-17 16:33:42 -0300

    finalização do arquivo do datePicker

--- engine/date_dialog.py
+++ engine/date_dialog.py
@@ -12,4 +12,31 @@ class DateDialog(QDialog):
         # criação do Widgets
         self.layout = QVBoxLayout(self)
 
-        
+        # Usando o QDateEdite como um pop-up
+        self.date_edit = QDateEdit(self, calendarPopup = True)
+        self.date_edit.setDisplayFormat("dd-MM-yyyy") # formato de exibição
+
+        # data maxima
+        self.date_edit.setMaximumDate(QDate.currentDate())
+        # data inicial padrao
+        self.date_edit.setDate(QDate.currentDate().addYears(-18))
+
+        # melhora a aparencia da font
+        font = self.date_edit.font()
+        font.setPointSize(12)
+        self.date_edit.setFont(font)
+
+        self.buttonBox = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel)
+
+        # conexao dos sinais
+        self.buttonBox.accepted.connect(self.accepted)
+        self.buttonBox.rejected.connect(self.rejected)
+
+        # adicionando os widgets ao layout
+        self.layout.addWidget(self.date_edit)
+        self.layout.addWidget(self.buttonBox)
+    
+    def selected_date(self):
+        """Retorna a data que o usuario retornou"""
+        # O método para pegar a data de um QDateEdit é .date()
+        return self.date_edit.date()
